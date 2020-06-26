889613e1ed6dff22b1b3ba25ec7ff0b6016e5bdf
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2020-06-26 15:31:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,23 @@ def task3() :
     
 def task4() : 
     return 0xABAD1DEA
+
+def task5() :
+    return 0xABAD1DEA
+
+import markengine
+
+testdata = [ 
+  # function, nargs, arg1, arg2, expect
+  (task5, 0, 2, type(1)),
+  (task1, 1, 3, 1, type(1)), 
+  (task1, 1, 2, 1, (type (""))),
+  (task1, 1, 2, 2, (type (1))),
+  (task2, 1, 1, 2, type (1)),
+  (task2, 1, 4, 8, type (1)),
+  (task3, 1, "Hello", 4, type(1)),
+  (task4, 1, "Hello", 4, type(1))
+]
+
+# Run the markengine
+score = markengine.mark (testdata)
