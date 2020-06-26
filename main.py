0c2178dea71c541cb159a080f15b02881165c11c
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2020-06-26 15:10:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 
+# Data Representation One
+#
+
+
+def task1() :
+    return 0xABAD1DEA  
+    
+def task2() :
+    return 0xABAD1DEA
+    
+def task3() :
+    return 0xABAD1DEA
+    
+def task4() : 
+    return 0xABAD1DEA
---
+++ markengine.py
@@ -0,0 +1,77 @@
+# 
+# 
+import sys
+import inspect
+import dis
+
+# Check if a function has been implemented
+
+def implemented (func, testno) :
+
+  code = dis.Bytecode (func)
+  for i in code :
+    if (i.argval == 0xdeadbeef) :
+      print ("FAIL: [", testno, "] NOT IMPLEMENTED: ", func.__name__)
+      return False
+    return True
+
+def runtest (test, testno) :
+
+  res = -1
+  passed = True
+  func = test[0]
+  try: 
+    nargs = test[1] 
+    if nargs == 0 :
+      res = func ()
+    elif (nargs == 1) :
+      res = func (test[2])
+    elif (nargs == 2) :
+      res = func (test[2], test[3])
+    else :
+        print ("MARK ENGINE: [", str (testno), "] Illegal arg count for ", func.__name__)
+        passed = False
+  
+  except:
+    print("FAIL: [", testno, "] EXCEPTION: ", func.__name__, sys.exc_info()[0])
+    passed = False
+
+  return passed, nargs, res
+
+def markengine () :
+  return (mark (testdata))
+
+
+def mark (data) :
+
+  count = 0
+  testno = -1
+  for test in data :
+    testno += 1
+    func = test[0]
+    if (not implemented (func, testno)) :
+      continue
+
+    if (not inspect.isfunction (func)) :
+      print ("FAIL: NOT IMPLEMENTED: ", func.__name__)
+      continue
+
+    passed, nargs, res = runtest (test, testno)
+    if (passed == False) :
+      # Exception or illegal args
+      continue
+    expected = test[nargs + 2]
+    expectedtype = test[nargs + 3]
+    if (type (res) != expectedtype) :
+      print ("FAIL: [", str (testno), "] RETURN TYPE ERROR: ", func.__name__, "Expected: ", expectedtype, "Got: ", type(res))
+    elif (res != expected) :
+      print ("FAIL: [", str (testno), "] INCORRECT RETURN VALUE: ", func.__name__, "Expected: ", expected, "Got: ", res)
+    else :
+      count += 1
+
+  # Print out the score    
+  
+  outof = len (data)
+  percentage = (count / outof) * 100
+  print ("\nScore: ", count, "out of", outof, "= ", str(percentage)+"%" )
+  return count
