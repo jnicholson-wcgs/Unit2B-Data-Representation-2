98b6c73be1c80c46b1ae6b04f97cd8fbc3e5e928
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2020-06-28 06:02:40 +0100

    Update markengine.py
    Updated with clearer error messages for failed tests

--- markengine.py
+++ markengine.py
@@ -20,14 +20,17 @@ def runtest (test, testno) :
   res = -1
   passed = True
   func = test[0]
+  fcall = func.__name__ + "()"
   try: 
     nargs = test[1] 
     if nargs == 0 :
       res = func ()
     elif (nargs == 1) :
       res = func (test[2])
+      fcall = func.__name__ + '(' + str (test[2]) + ")"
     elif (nargs == 2) :
       res = func (test[2], test[3])
+      fcall = func.__name__ + "(" + str (test[2]) + ", " + str(test[3]) + ")"
     else :
         print ("MARK ENGINE: [", str (testno), "] Illegal arg count for ", func.__name__)
         passed = False
@@ -36,7 +39,7 @@ def runtest (test, testno) :
     print("FAIL: [", testno, "] EXCEPTION: ", func.__name__, sys.exc_info()[0])
     passed = False
 
-  return passed, nargs, res
+  return passed, nargs, res, fcall
 
 def markengine () :
   return (mark (testdata))
@@ -56,16 +59,18 @@ def mark (data, outof) :
       print ("FAIL: NOT IMPLEMENTED: ", func.__name__)
       continue
 
-    passed, nargs, res = runtest (test, testno)
+    passed, nargs, res, fcall = runtest (test, testno)
     if (passed == False) :
       # Exception or illegal args
       continue
+    
     expected = test[nargs + 2]
     expectedtype = test[nargs + 3]
+    t = "FAILED TEST [" + str(testno) + "]: "
     if (type (res) != expectedtype) :
-      print ("FAIL: [", str (testno), "] RETURN TYPE ERROR: ", func.__name__, "Expected: ", expectedtype, "Got: ", type(res))
+      print (t, "RETURN TYPE: called ", fcall, "Expected: ", expectedtype, "Got: ", type(res))
     elif (res != expected) :
-      print ("FAIL: [", str (testno), "] INCORRECT RETURN VALUE: ", func.__name__, "Expected: ", expected, "Got: ", res)
+      print (t, "RETURN VALUE: called ", fcall, "Expected: ", expected, "Got: ", res)
     else :
       count += 1
 
